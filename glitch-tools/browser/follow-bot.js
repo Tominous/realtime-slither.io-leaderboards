2baf2cbbe08693fe60d10e4ae9583c1d77e982f4
Glitch (rsl) <none>
2019-06-06 20:46:29 +0000

    follow Glitch bot: move to glitch-tools/browser/follow-bot follow Glitch bot: use browserify

---
+++ glitch-tools/browser/follow-bot.js
@@ -0,0 +1,35 @@
+;(function() {
+  let messages = require('./messages')
+
+  ;(function connectToBot() {
+    let socket = new WebSocket('ws://rsl.glitch.me')
+
+    socket.binaryType = 'arraybuffer'
+
+    socket.onmessage = function(event) {
+      if (snake === null) return
+
+      let decoded = messages.botPositionAndLength.decode(
+        Buffer.from(event.data)
+      )
+      if (decoded.server !== `${bso.ip}:${bso.po}`) return
+
+      xm = decoded.x - snake.xx
+      ym = decoded.y - snake.yy
+
+      let distance = Math.abs(xm | 0) + Math.abs(ym | 0)
+
+      document.title = `Distance: ${distance}`
+    }
+
+    socket.onclose = function() {
+      setTimeout(function() {
+        connectToBot()
+      }, 1000)
+    }
+
+    socket.onerror = function() {
+      socket.close()
+    }
+  })()
+})()
